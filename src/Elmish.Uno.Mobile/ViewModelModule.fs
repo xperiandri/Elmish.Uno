7d38f110f395882b5f95fe1edc372e7194ad6355
XperiAndri <xperiandri@live.ru>
2021-01-01 22:58:19 +0200

    Implemented view model loop start and fixed design instance

--- src/Elmish.Uno.Mobile/ViewModelModule.fs
+++ src/Elmish.Uno.Mobile/ViewModelModule.fs
@@ -1,5 +1,52 @@
-﻿module Elmish.Uno.ViewModel
+﻿[<RequireQualifiedAccess>]
+module Elmish.Uno.ViewModel
+
+open System
+open Windows.UI.Xaml
+open Windows.UI.Core
+
+open Elmish
+open Elmish.Uno
+
+/// Start Elmish dispatch loop
+let startLoop
+    (config: ElmConfig)
+    (element: FrameworkElement)
+    (programRun: Program<'t, 'model, 'msg, Binding<'model, 'msg> list> -> unit)
+    (program: Program<'t, 'model, 'msg, Binding<'model, 'msg> list>) =
+
+    let mutable lastModel = None
+
+    let setState model dispatch =
+        match lastModel with
+        | None ->
+            let bindings = Program.view program model dispatch
+            let vm = ViewModel<'model,'msg>(model, dispatch, bindings, config, "main")
+            element.DataContext <- box vm
+            lastModel <- Some vm
+        | Some vm ->
+            vm.UpdateModel model
+
+    let uiDispatch (innerDispatch: Dispatch<'msg>) : Dispatch<'msg> =
+        fun msg ->
+        let doDispatch = fun () ->
+            Console.WriteLine "Dispatch"
+            innerDispatch msg
+        element.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, fun () -> doDispatch()) |> ignore
+
+    program
+    |> Program.withSetState setState
+    |> Program.withSyncDispatch uiDispatch
+    |> programRun
+
+let StartLoop (config, element, programRun : Action<Program<'t, 'model, 'msg, Binding<'model, 'msg> list>>, program) =
+    startLoop config element (FuncConvert.FromAction programRun) program
+
 
 /// Creates a design-time view model using the given model and bindings.
-let designInstance (model: 'model) (bindings: Binding<'model, 'msg> list) =
-  ViewModel(model, ignore, bindings, ElmConfig.Default, "main") |> box
+let designInstance (model: 'model) (program: Program<'t, 'model, 'msg, Binding<'model, 'msg> list>) =
+    let bindings = Program.view program model ignore
+    ViewModel (model, ignore, bindings, ElmConfig.Default, "main") |> box
+
+let DesignInstance (model: 'model, program: Program<'t, 'model, 'msg, Binding<'model, 'msg> list>) =
+    designInstance model program
