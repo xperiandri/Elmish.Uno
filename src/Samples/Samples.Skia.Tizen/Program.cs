b9f32f64caec6865393983ae41bd32e40fd3fb9d
XperiAndri <xperiandri@live.ru>
2021-01-03 15:49:47 +0200

    Added Uno app projects

---
+++ src/Samples/Samples.Skia.Tizen/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tizen.Applications;
+
+using Uno.UI.Runtime.Skia;
+
+namespace Elmish.Uno.Samples.Skia.Tizen
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new Samples.App(), args);
+            host.Run();
+        }
+    }
+}
