adc42ecff4fedccf5be341955280067489a28434
XperiAndri <xperiandri@live.ru>
2021-01-03 15:49:47 +0200

    Added Uno app projects

---
+++ src/Samples/Samples.macOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace Elmish.Uno.Samples
+{
+	public partial class App
+	{
+		private static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.SharedApplication.Delegate = new App();
+			NSApplication.Main(args);
+		}
+	}
+}
+
