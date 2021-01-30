637cbc52c7050a91884f0db36c0e5559642d02d2
dim-37 <otvelnik12@gamil.com>
2021-01-31 01:44:27 +0200

    Implemented basic navigation

---
+++ src/Elmish.Uno/Navigation.fs
@@ -0,0 +1,21 @@
+﻿namespace Elmish.Uno.Navigation
+
+open System
+open System.Collections.Generic
+
+type public NavigationParameter   = KeyValuePair<string, obj>
+type public INavigationParameters = IReadOnlyDictionary<string, obj>
+
+type public INavigationService =
+  abstract Navigate           : name : string                                              -> bool
+  abstract Navigate           : name : string *  navigationParams : INavigationParameters  -> bool
+
+  abstract GoBack             : unit                                                       -> unit
+  abstract GoForward          : unit                                                       -> unit
+  abstract GetNavigationState : unit                                                       -> string
+  abstract SetNavigationState : navigationState : string                                   -> unit
+
+  abstract CacheSize          : int
+  abstract BackStackDepth     : int
+  abstract CanGoBack          : bool
+  abstract CanGoForward       : bool
