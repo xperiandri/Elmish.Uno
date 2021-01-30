791c524af0e419252689db3979f5d75fab3def2a
dim-37 <otvelnik12@gamil.com>
2021-01-31 01:44:27 +0200

    Implemented basic navigation

---
+++ src/Elmish.Uno.Mobile/Navigation.fs
@@ -0,0 +1,40 @@
+﻿namespace Elmish.Uno.Navigation
+
+open System
+open System.Collections.Generic
+open FSharp.Collections.Immutable
+open Windows.UI.Xaml.Controls
+
+type public NavigationService (frame : Frame, pageMap : IReadOnlyDictionary<string, Type>) =
+
+  new (frame, pageMap : KeyValuePair<string, Type> seq) =
+    let pageMap' = pageMap
+                   |> Seq.fold
+                         (fun (b : HashMap<string, Type>.Builder) p -> b.Add (p.Key, p.Value); b)
+                         (HashMap<string, Type>.Empty.ToBuilder())
+    NavigationService (frame,  pageMap'.ToImmutable())
+
+  interface INavigationService with
+    member this.BackStackDepth = frame.BackStackDepth
+    member this.CacheSize      = frame.CacheSize
+    member this.CanGoBack      = frame.CanGoBack
+    member this.CanGoForward   = frame.CanGoForward
+
+
+    member this.GetNavigationState (): string =
+      frame.GetNavigationState ()
+
+    member this.SetNavigationState(navigationState: string): unit =
+      frame.SetNavigationState (navigationState)
+
+    member this.GoBack (): unit =
+      frame.GoBack ()
+
+    member this.GoForward (): unit =
+      frame.GoForward ()
+
+    member this.Navigate (name: string, navigationParams: INavigationParameters): bool =
+      frame.Navigate (pageMap.[name], navigationParams)
+
+    member this.Navigate (name: string): bool =
+      frame.Navigate (pageMap.[name], null)
---
+++ src/Elmish.Uno.Uwp/NavigationService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+
+namespace Elmish.Uno.Navigation
+{
+    public class NavigationService : INavigationService
+    {
+        private readonly Frame frame;
+        private readonly IReadOnlyDictionary<string, Type> pageMap;
+
+        public NavigationService(Frame frame, IReadOnlyDictionary<string, Type> pageMap)
+        {
+            this.frame = frame;
+            this.pageMap = pageMap;
+        }
+
+        public NavigationService(Frame frame, IEnumerable<KeyValuePair<string, Type>> pageMap)
+        {
+            this.frame = frame;
+            this.pageMap = pageMap.Aggregate(
+                    ImmutableDictionary<string, Type>.Empty.ToBuilder(),
+                    (builder, kvp) => { builder.Add(kvp.Key, kvp.Value); return builder; })
+                .ToImmutable();
+        }
+
+        public int CacheSize => frame.CacheSize;
+
+        public int BackStackDepth => frame.BackStackDepth;
+
+        public bool CanGoBack => frame.CanGoBack;
+
+        public bool CanGoForward => frame.CanGoForward;
+
+        public string GetNavigationState() => frame.GetNavigationState();
+        public void SetNavigationState(string navigationState) => frame.SetNavigationState(navigationState);
+        public void GoBack() => frame.GoBack();
+        public void GoForward() => frame.GoForward();
+        public bool Navigate(string name) => frame.Navigate(pageMap[name], null);
+        public bool Navigate(string name, IReadOnlyDictionary<string, object> navigationParams) => frame.Navigate(pageMap[name], navigationParams);
+    }
+}
