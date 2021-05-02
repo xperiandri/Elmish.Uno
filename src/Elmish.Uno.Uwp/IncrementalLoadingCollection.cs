e7abbb2d6b271b2920c64be3a6a873b8556268ba
Andrii Chebukin <xperiandri@live.ru>
2021-05-03 01:31:15 +0300

    Implemented IncrementalLoadingCollection

---
+++ src/Elmish.Uno.Uwp/IncrementalLoadingCollection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+using Windows.Foundation;
+using Windows.UI.Xaml.Data;
+
+namespace Elmish.Uno
+{
+    internal class IncrementalLoadingCollection<T> : ObservableCollection<T>, ISupportIncrementalLoading
+    {
+
+        private readonly Func<bool> hasMoreItems;
+        private readonly Action<uint, TaskCompletionSource<uint>> loadMoreItems;
+
+        public IncrementalLoadingCollection(Func<bool> hasMoreItems, Action<uint, TaskCompletionSource<uint>> loadMoreItems)
+        {
+            this.hasMoreItems = hasMoreItems;
+            this.loadMoreItems = loadMoreItems;
+        }
+
+        public IncrementalLoadingCollection(IEnumerable<T> collection, Func<bool> hasMoreItems, Action<uint, TaskCompletionSource<uint>> loadMoreItems) : base(collection)
+        {
+            this.hasMoreItems = hasMoreItems;
+            this.loadMoreItems = loadMoreItems;
+        }
+
+        public IncrementalLoadingCollection(List<T> list, Func<bool> hasMoreItems, Action<uint, TaskCompletionSource<uint>> loadMoreItems) : base(list)
+        {
+            this.hasMoreItems = hasMoreItems;
+            this.loadMoreItems = loadMoreItems;
+        }
+
+        public bool HasMoreItems => hasMoreItems();
+
+        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+        {
+            async Task<LoadMoreItemsResult> LoadMoreItemsAsync ()
+            {
+                var tcs = new TaskCompletionSource<uint>();
+                loadMoreItems(count, tcs);
+                var actualCount = await tcs.Task;
+                return new LoadMoreItemsResult { Count = actualCount };
+            }
+            return LoadMoreItemsAsync().AsAsyncOperation();
+        }
+    }
+}
