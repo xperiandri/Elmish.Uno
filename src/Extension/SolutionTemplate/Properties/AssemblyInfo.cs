b2cc1a3a30fccc116d58dff26c4371b29589ba2c
dim-37 <otvelnik12@gamil.com>
2021-05-18 18:51:35 +0300

    Re named extension

--- src/Extension/SolutionTemplate/Properties/AssemblyInfo.cs
+++ src/Extension/SolutionTemplate/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Elmish.Uno.Templates")]
+[assembly: AssemblyTitle("SolutionTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Elmish.Uno.Templates")]
+[assembly: AssemblyProduct("SolutionTemplate")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
