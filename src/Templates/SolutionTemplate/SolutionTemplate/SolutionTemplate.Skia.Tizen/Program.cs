4e6cefc7061e5a24b1f5eb3cddad9c1195d6618c
dim-37 <otwelnik@secretcircle.solutions>
2021-05-26 18:05:00 +0300

    Fixed template

---
+++ src/Templates/SolutionTemplate/SolutionTemplate/SolutionTemplate.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace SolutionTemplate.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new SolutionTemplate.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ src/Templates/SolutionTemplate/SolutionTemplate/SolutionTemplate.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace SolutionTemplate.Skia.Gtk
+{
+}
