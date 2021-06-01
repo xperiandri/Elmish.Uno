a7a10e2af0a4ae0884665c6ffa0dda99fb800c87
dim-37 <otwelnik@secretcircle.solutions>
2021-06-01 14:32:12 +0300

    Fixed template in Release

---
+++ src/Templates/SolutionTemplate/SolutionTemplate.Wasm/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SolutionTemplate.Wasm
+{
+    public partial class Program
+    {
+#pragma warning disable IDE1006 // Naming Styles
+        private static App app;
+#pragma warning restore IDE1006 // Naming Styles
+
+#pragma warning disable RCS1163 // Unused parameter.
+#pragma warning disable CA1801 // Review unused parameters
+        private static int Main(string[] args)
+#pragma warning restore CA1801 // Review unused parameters
+#pragma warning restore RCS1163 // Unused parameter.
+        {
+            Windows.UI.Xaml.Application.Start(_ => app = new App());
+
+            return 0;
+        }
+    }
+}
