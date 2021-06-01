0386e41cf893ee973d0d254bc1b41e7682791dd3
dim-37 <otwelnik@secretcircle.solutions>
2021-06-01 18:58:08 +0300

    Fixed PR Comments

--- src/Templates/SolutionTemplate/SolutionTemplate.iOS/Properties/AssemblyInfo.cs
+++ src/Templates/SolutionTemplate/SolutionTemplate.iOS/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using SolutionTemplate;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle(AssemblyInfo.Product + " iOS")]
+[assembly: AssemblyTitle(AssemblyInfo.Product + "  iOS app")]
 /// TODO: Modify description per platform
 [assembly: AssemblyDescription(AssemblyInfo.Description)]
 [assembly: AssemblyConfiguration("")]
