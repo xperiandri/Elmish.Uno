e7c3074bc7c639197dd6d023cf3ca090cfc75849
Andrii Chebukin <xperiandri@live.ru>
2021-06-02 15:00:58 +0300

    Suppressed missing warnings and added assembly names

--- src/Templates/SolutionTemplate/SolutionTemplate.Skia.Wpf.Host/Properties/Settings.Designer.cs
+++ src/Templates/SolutionTemplate/SolutionTemplate.Skia.Wpf.Host/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SolutionTemplate.Properties {
+namespace SolutionTemplate.WPF.Host.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
