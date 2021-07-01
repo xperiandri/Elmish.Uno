eb122ccfc391eb632957e8ed7806b39610db320d
Andrii Chebukin <xperiandri@live.ru>
2021-07-01 14:58:11 +0300

    Added sample configuration code for IOptions pattern usage

---
+++ src/Templates/SolutionTemplate/SolutionTemplate/Configuration.fs
@@ -0,0 +1,53 @@
+﻿[<CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
+module SolutionTemplate.Configuration
+
+open System
+open System.Linq
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Hosting
+open Xamarin.Essentials
+
+module Environments =
+    let LocalAPI = "LocalAPI"
+
+let configure (ctx : HostBuilderContext) (builder : IConfigurationBuilder) =
+
+    let environmentName = ctx.HostingEnvironment.EnvironmentName
+
+    let inMemoryCollection = seq {
+
+        yield!
+            match environmentName with
+            | name when name.Equals(Environments.LocalAPI, StringComparison.CurrentCultureIgnoreCase) ->
+                let endpoint =
+                    if DeviceInfo.Platform = DevicePlatform.Android
+                    then "http://10.0.2.2:7071/GraphQL"
+                    else "http://localhost:7071/GraphQL"
+                seq {
+                    yield struct("GraphQL:EndPoint", endpoint)
+                }
+            | name when name.Equals(Environments.Development, StringComparison.CurrentCultureIgnoreCase) ->
+                seq {
+                    yield struct("GraphQL:EndPoint", "https://host.net/GraphQL")
+                }
+            | name when name.Equals(Environments.Production, StringComparison.CurrentCultureIgnoreCase) ->
+                seq {
+                    yield struct("GraphQL:EndPoint", "https://host.net/GraphQL")
+                }
+            | _ -> raise <| NotSupportedException ()
+
+        //yield struct("AzureMaps:Key", BingAutocomplete.key)
+    }
+
+    builder.AddInMemoryCollection (inMemoryCollection.ToDictionary(fstv, sndv)) |> ignore
+    ()
+
+[<CLIMutable>]
+type GraphQLSettings =
+    { EndPoint : string }
+
+open Microsoft.Extensions.DependencyInjection
+
+type IServiceCollection with
+    member services.ConfigureSolutionTemplateOptions (configuration : IConfiguration) =
+        services.Configure<GraphQLSettings>(configuration.GetSection("GraphQL"))
--- src/Templates/SolutionTemplate/SolutionTemplate/Host.fs
+++ src/Templates/SolutionTemplate/SolutionTemplate/Host.fs
@@ -8,62 +8,24 @@ open System.Linq
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
-open Microsoft.Extensions.Options
 open Xamarin.Essentials
 
+open SolutionTemplate.Configuration
 open SolutionTemplate.Programs
 
 let configureHost (builder : IConfigurationBuilder) =
 
     if DeviceInfo.Platform <> DevicePlatform.UWP then
-        let root = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "root")
-        Directory.CreateDirectory(root) |> ignore
+        let root = Path.Combine (System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "root")
+        Directory.CreateDirectory root |> ignore
         let inMemoryCollection = seq {
             struct(HostDefaults.ContentRootKey, root)
         }
-        builder.AddInMemoryCollection (inMemoryCollection.ToDictionary((fun struct(k,_) -> k), (fun struct(_,v) -> v))) |> ignore
-
-let configureConfiguration (ctx : HostBuilderContext) (builder : IConfigurationBuilder) =
-
-    // TODO: Use some other way usable for conditional builds
-    let environmentName = Environments.Development
-    ctx.HostingEnvironment.EnvironmentName <- environmentName
-    //let environmentName = ctx.HostingEnvironment.EnvironmentName
-    //let inMemoryCollection = seq {
-
-    //    //if not (DeviceInfo.Platform = DevicePlatform.UWP) then
-    //    //    let root = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "root")
-    //    //    Directory.CreateDirectory(root)
-    //    //    yield struct(HostDefaults.ContentRootKey, root)
-
-    //    yield!
-    //        match environmentName with
-    //        | name when name.Equals(Environments.DevelopmentLocal, StringComparison.CurrentCultureIgnoreCase) ->
-    //            let endpoint =
-    //                if DeviceInfo.Platform = DevicePlatform.Android
-    //                    then "http://10.0.2.2:7071/GraphQL"
-    //                    else "http://localhost:7071/GraphQL"
-    //            seq {
-    //                yield struct("GraphQL:Endpoint", endpoint)
-    //            }
-    //        | name when name.Equals(Environments.Development, StringComparison.CurrentCultureIgnoreCase) ->
-    //            seq {
-    //                yield struct("GraphQL:Endpoint", "https://host.net/GraphQL")
-    //            }
-    //        | name when name.Equals("Production", StringComparison.CurrentCultureIgnoreCase) ->
-    //            seq {
-    //                yield struct("GraphQL:Endpoint", "https://host.net/GraphQL")
-    //            }
-    //        | _ -> raise <| NotSupportedException ()
-
-    //    yield struct("AzureMaps:Key", BingAutocomplete.key)
-    //}
-
-    //builder.AddInMemoryCollection (inMemoryCollection.ToDictionary((fun struct(k,_) -> k), (fun struct(_,v) -> v))) |> ignore
-    //()
+        builder.AddInMemoryCollection (inMemoryCollection.ToDictionary(fstv, sndv)) |> ignore
 
 let configureServices (ctx : HostBuilderContext) (services : IServiceCollection) =
     services
+        .ConfigureSolutionTemplateOptions(ctx.Configuration)
         .AddScoped<Notification.Program>()
         .AddScoped<App.Program>()
     |> ignore
@@ -71,9 +33,21 @@ let configureServices (ctx : HostBuilderContext) (services : IServiceCollection)
 [<CompiledName("CreateDefaultBuilder")>]
 let createDefaultBuilder () =
 
+    let environmentName =
+    #if PRODUCTION
+        Environments.Production
+    #elif STAGING
+        Environments.Staging
+    #elif LOCALAPI
+        Environments.LocalAPI
+    #else
+        Environments.Development
+    #endif
+
     HostBuilder()
+        .UseEnvironment(environmentName)
         .ConfigureHostConfiguration(Action<_>(configureHost))
-        .ConfigureAppConfiguration(configureConfiguration)
+        .ConfigureAppConfiguration(Configuration.configure)
         .ConfigureServices(configureServices)
 
 [<CompiledName("ElmConfig")>]
