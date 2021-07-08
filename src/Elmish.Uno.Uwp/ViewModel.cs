4071eea98b539af907baad7d724f177e22fe2567
Andrii Chebukin <xperiandri@live.ru>
2021-07-08 16:16:48 +0300

    Fixed ICustomProperty bindings initialization

--- src/Elmish.Uno.Uwp/ViewModel.cs
+++ src/Elmish.Uno.Uwp/ViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Immutable;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -44,7 +45,18 @@ namespace Elmish.Windows
 
     internal class ViewModel<TModel, TMsg> : Uno.ViewModel<TModel, TMsg>, ICustomPropertyProvider
     {
-        public ViewModel(TModel initialModel, FSharpFunc<TMsg, Unit> dispatch, FSharpList<Binding<TModel, TMsg>> bindings, ElmConfig config, string propNameChain) : base(initialModel, dispatch, bindings, config, propNameChain) { }
+        private readonly ImmutableDictionary<string, string> bindings;
+
+        public ViewModel(TModel initialModel, FSharpFunc<TMsg, Unit> dispatch, FSharpList<Binding<TModel, TMsg>> bindings, ElmConfig config, string propNameChain) : base(initialModel, dispatch, bindings, config, propNameChain)
+        {
+            string GetBindingType(Binding<TModel, TMsg> binding)
+            {
+                //var (info, _) = FSharpValue.GetUnionFields(binding.Data, typeof(BindingData<TModel, TMsg>), FSharpOption<BindingFlags>.Some(BindingFlags.NonPublic));
+                return binding.Data.GetType().Name;
+            }
+
+            this.bindings = bindings.ToImmutableDictionary(b => b.Name, GetBindingType);
+        }
 
         public override Uno.ViewModel<object, object> Create(object initialModel, FSharpFunc<object, Unit> dispatch, FSharpList<Binding<object, object>> bindings, ElmConfig config, string propNameChain)
          => new ViewModel<object, object>(initialModel, dispatch, bindings, config, propNameChain);
@@ -53,36 +65,35 @@ namespace Elmish.Windows
         {
             if (name == "CurrentModel") return new DynamicCustomProperty<ViewModel<TModel, TMsg>, object>(name, vm => vm.CurrentModel);
             if (name == "HasErrors") return new DynamicCustomProperty<ViewModel<TModel, TMsg>, bool>(name, vm => ((INotifyDataErrorInfo)vm).HasErrors);
-            if (!this.Bindings.TryGetValue(name, out var binding)) Debugger.Break();
-            switch (binding)
+            if (!bindings.TryGetValue(name, out var bindingType)) Debugger.Break();
+            switch (bindingType)
             {
-                case VmBinding<TModel, TMsg>.OneWay oneWay:
-                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, object>(name, vm => vm.TryGetMember(oneWay));
-                case VmBinding<TModel, TMsg>.OneWayLazy oneWayLazy:
-                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, object>(name, vm => vm.TryGetMember(oneWayLazy));
-                case VmBinding<TModel, TMsg>.OneWaySeq oneWaySeq:
+                case nameof(BindingData<TModel, TMsg>.OneWayData):
+                case nameof(BindingData<TModel, TMsg>.OneWayLazyData):
+                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, object>(name, vm => vm.TryGetMember(vm.Bindings[name]));
+                case nameof(BindingData<TModel, TMsg>.OneWaySeqLazyData):
                     return new DynamicCustomProperty<ViewModel<TModel, TMsg>, ObservableCollection<object>>(name,
-                        vm => (ObservableCollection<object>)vm.TryGetMember(oneWaySeq));
-                case VmBinding<TModel, TMsg>.TwoWay twoWay:
-                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, object>(name, vm => vm.TryGetMember(twoWay), (vm, value) => vm.TrySetMember(value, twoWay));
-                case VmBinding<TModel, TMsg>.TwoWayValidate twoWayValidate:
-                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, object>(name, vm => vm.TryGetMember(twoWayValidate), (vm, value) => vm.TrySetMember(value, twoWayValidate));
-                case VmBinding<TModel, TMsg>.Cmd cmd:
-                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, ICommand>(name, vm => vm.TryGetMember(cmd) as ICommand);
-                case VmBinding<TModel, TMsg>.CmdParam cmdParam:
-                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, object>(name, vm => vm.TryGetMember(cmdParam));
-                case VmBinding<TModel, TMsg>.SubModel subModel:
-                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, ViewModel<object, object>>(name,
-                        vm => vm.TryGetMember(subModel) as ViewModel<object, object>);
-                case VmBinding<TModel, TMsg>.SubModelSeq subModelSeq:
-                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, ObservableCollection<Uno.ViewModel<object, object>>>(name,
-                        vm => (ObservableCollection<Uno.ViewModel<object, object>>)vm.TryGetMember(subModelSeq));
-                case VmBinding<TModel, TMsg>.SubModelSelectedItem subModelSelectedItem:
-                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, ViewModel<object, object>>(name,
-                        vm => (ViewModel<object, object>) vm.TryGetMember(subModelSelectedItem));
-                case VmBinding<TModel, TMsg>.Cached cached:
+                        vm => (ObservableCollection<object>)vm.TryGetMember(vm.Bindings[name]));
+                case nameof(BindingData<TModel, TMsg>.TwoWayData):
                     return new DynamicCustomProperty<ViewModel<TModel, TMsg>, object>(name,
-                        vm => vm.TryGetMember(cached), (vm, value) => vm.TrySetMember(value, cached));
+                        vm => vm.TryGetMember(vm.Bindings[name]), (vm, value) => vm.TrySetMember(value, vm.Bindings[name]));
+                case nameof(BindingData<TModel, TMsg>.TwoWayValidateData):
+                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, object>(name,
+                        vm => vm.TryGetMember(vm.Bindings[name]), (vm, value) => vm.TrySetMember(value, vm.Bindings[name]));
+                case nameof(BindingData<TModel, TMsg>.CmdData):
+                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, ICommand>(name,
+                        vm => vm.TryGetMember(vm.Bindings[name]) as ICommand);
+                case nameof(BindingData<TModel, TMsg>.CmdParamData):
+                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, object>(name, vm => vm.TryGetMember(vm.Bindings[name]));
+                case nameof(BindingData<TModel, TMsg>.SubModelData):
+                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, ViewModel<object, object>>(name,
+                        vm => vm.TryGetMember(vm.Bindings[name]) as ViewModel<object, object>);
+                case nameof(BindingData<TModel, TMsg>.SubModelSeqData):
+                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, ObservableCollection<Uno.ViewModel<object, object>>>(name,
+                        vm => (ObservableCollection<Uno.ViewModel<object, object>>)vm.TryGetMember(vm.Bindings[name]));
+                case nameof(BindingData<TModel, TMsg>.SubModelSelectedItemData):
+                    return new DynamicCustomProperty<ViewModel<TModel, TMsg>, ViewModel<object, object>>(name,
+                        vm => (ViewModel<object, object>)vm.TryGetMember(vm.Bindings[name]));
                 default:
                     return null;
                     //throw new NotSupportedException();
@@ -122,10 +133,10 @@ namespace Elmish.Uno
             FSharpRef<FSharpOption<ViewModel<TModel, TMsg>>> lastModel = new FSharpRef<FSharpOption<ViewModel<TModel, TMsg>>>(null);
             FSharpFunc<FSharpFunc<TMsg, Unit>, FSharpFunc<TMsg, Unit>> syncDispatch =
               FuncConvert.FromAction(MakeSyncDispatch<TMsg>(element));
-            var setSate = FuncConvert.FromAction(MakeSetState(config, element, program, lastModel));
+            var setState = FuncConvert.FromAction(MakeSetState(config, element, program, lastModel));
             programRun.Invoke(
                 ProgramModule.withSyncDispatch(syncDispatch,
-                  ProgramModule.withSetState(setSate, program)));
+                  ProgramModule.withSetState(setState, program)));
         }
 
         public static void StartLoop<T, TModel, TMsg>(ElmConfig config, FrameworkElement element, Action<T, Program<T, TModel, TMsg, FSharpList<Binding<TModel, TMsg>>>> programRun, Program<T, TModel, TMsg, FSharpList<Binding<TModel, TMsg>>> program, T arg)
@@ -133,11 +144,11 @@ namespace Elmish.Uno
             FSharpRef<FSharpOption<ViewModel<TModel, TMsg>>> lastModel = new FSharpRef<FSharpOption<ViewModel<TModel, TMsg>>>(null);
             FSharpFunc<FSharpFunc<TMsg, Unit>, FSharpFunc<TMsg, Unit>> syncDispatch =
               FuncConvert.FromAction(MakeSyncDispatch<TMsg>(element));
-            var setSate = FuncConvert.FromAction(MakeSetState(config, element, program, lastModel));
+            var setState = FuncConvert.FromAction(MakeSetState(config, element, program, lastModel));
 
             programRun.Invoke(arg,
                 ProgramModule.withSyncDispatch(syncDispatch,
-                  ProgramModule.withSetState(setSate, program)));
+                  ProgramModule.withSetState(setState, program)));
         }
 
 
